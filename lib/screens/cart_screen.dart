8d98abb0f991a0e87381c6a5d4db816e45ccd138
Giovanni Panasiti <giova.panasiti@gmail.com>
2020-04-14 14:45:09 +0200

    add checkout button at the bottom sheet of cart

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -113,19 +113,85 @@ class _CartScreenState extends State<CartScreen> {
 
   @override
   Widget build(BuildContext context) {
+    double totalPrice = 0;
+
+    currentUser.cart.forEach(
+        (Order order) => totalPrice += order.quantity * order.food.price);
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Cart (${currentUser.cart.length})'),
       ),
       body: ListView.separated(
           itemBuilder: (BuildContext context, int index) {
-            Order order = currentUser.cart[index];
-            return _buildCartItem(order);
+            if (index < currentUser.cart.length) {
+              Order order = currentUser.cart[index];
+              return _buildCartItem(order);
+            }
+            return Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        'Estimated Devlivery Time',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600, fontSize: 20.0),
+                      ),
+                      Text(
+                        '25 minutes',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600, fontSize: 20.0),
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        'Total Cost',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600, fontSize: 20.0),
+                      ),
+                      Text(
+                        '\$${totalPrice.toStringAsFixed(2)}',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 20.0,
+                            color: Colors.green[700]),
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 80.0,)
+                ],
+              ),
+            );
           },
           separatorBuilder: (BuildContext context, int index) {
             return Divider(height: 1.0, color: Colors.grey);
           },
-          itemCount: currentUser.cart.length),
+          itemCount: currentUser.cart.length + 1),
+      bottomSheet: Container(
+          height: 100.0,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              boxShadow: [
+                BoxShadow(color: Colors.black26, offset: Offset(0, -1))
+              ]),
+          child: Center(
+            child: FlatButton(
+              child: Text('CHECKOUT',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 2.0)),
+              onPressed: () {},
+            ),
+          )),
     );
   }
 }
